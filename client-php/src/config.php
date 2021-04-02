84aed5bc0a54b4b0e750622e88a9749ccfbdf4e2
thejudge <thejudge@fortress.ch>
2021-04-02 12:03:45 +0200

    updated slim to 4 and added rest call test

--- client-php/src/config.php
+++ client-php/src/config.php
@@ -2,10 +2,10 @@
     $container = $app->getContainer();
 
     // monolog
-    $container['logger'] = function ($c) {
+    $container->set('logger', function ($c) {
         $settings = $c->get('settings')['logger'];
         $logger = new Monolog\Logger($settings['name']);
         $logger->pushProcessor(new Monolog\Processor\UidProcessor());
         $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
         return $logger;
-    };
+    });
--- client-php/src/routes.php
+++ client-php/src/routes.php
@@ -1,15 +1,17 @@
 <?php
     use \Psr\Http\Message\ServerRequestInterface as Request;
     use \Psr\Http\Message\ResponseInterface as Response;
+use Slim\App;
 
-    $app->post('/', function (Request $request, Response $response, array $args) {
+return function (App $app) {
+    $app->post('/client-php/', function (Request $request, Response $response, array $args) {
         try {
             // decode data from the caller
             $GLOBALS['body'] = json_decode($request->getBody());
             /**
              * Send to Socket
              */
-            \Ratchet\Client\connect('ws://127.0.0.1:8683')->then(function($conn) {
+            \Ratchet\Client\connect('ws://127.0.0.1:8080')->then(function($conn) {
                 $conn->send(
                     json_encode(array(
                         "user" => $GLOBALS['body']->user,
@@ -17,12 +19,17 @@
                     ))
                 );
                 $conn->close();
+                $this->get('logger')->debug("Sent messages successfully: user => " . $GLOBALS['body']->user);
             }, function ($e) {
-                $this->logger->info("Can not connect: {$e->getMessage()}");
+                $this->get('logger')->info("Can not connect: {$e->getMessage()}");
             });
-            return json_encode(array("status" => true));
+            echo json_encode(array("status" => true));
         } catch (Exception $e) {
-            $this->logger->info("Exception: ", $e->getMessage());
-            return json_encode(array("status" => false));
+            $this->get('logger')->info("Exception: ", $e->getMessage());
+            echo json_encode(array("status" => false)); 
         }
+        return $response;
     });
+};
+
+    
